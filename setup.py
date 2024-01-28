b5bf4d8c52e08d974b1378c9f5011f97ded97bae
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-01-28 20:50:58 +0330

    test(core): test the suitability of data/features-extractor

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.0.0.6.3"
+VERSION = "0.0.0.6.4"
 DESCRIPTION = "To be added in the future"
 
 
@@ -23,7 +23,7 @@ setuptools.setup(
     include_package_data=True,
     install_requires=[
         "ise_cdg_models @ git+https://github.com/ISE-Code-Documentation-Generators/models.git@feat/data-parallel",
-        "ise_cdg_data @ git+https://github.com/ISE-Code-Documentation-Generators/data.git",
+        "ise_cdg_data @ git+https://github.com/ISE-Code-Documentation-Generators/data.git@feat/features-extractor",
         "ise_cdg_utility @ git+https://github.com/ISE-Code-Documentation-Generators/utility.git",
     ],
     classifiers=[
