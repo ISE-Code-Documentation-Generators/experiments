071c8e60c21762c85311b6a5e72c56ca96e307e6
Ashkan Khademian <ashkan.khd.q@gmail.com>
2024-02-07 04:03:12 +0330

    fix: fix experiment

--- ise_cdg_experiments/cnn2rnn/experiment.py
+++ ise_cdg_experiments/cnn2rnn/experiment.py
@@ -10,15 +10,5 @@ from ise_cdg_experiments.interfaces import (
 
 class CNN2RNNExperiment(BaseExperiment):
 
-    def __init__(
-        self,
-        models: List[ExperimentalModelInterface],
-        device: device,
-        num_epochs: int,
-        loader: DataLoader,
-        trainer: CNN2RNNBatchTrainer,
-    ) -> None:
-        super().__init__(models, device, num_epochs, loader, trainer)
-
     def should_save(self, epoch: int) -> bool:
         return not ((epoch + 1) % 10)
