2ac258fd92d79ef3a4c9bc61ea29fe7f1315c8b0
Ashkan Khademian <ashkan.khd.q@gmail.com>
2024-02-11 14:18:47 +0330

    feat: change cnn-rnn experiment config

--- ise_cdg_experiments/cnn2rnn/experiment.py
+++ ise_cdg_experiments/cnn2rnn/experiment.py
@@ -4,4 +4,4 @@ from ise_cdg_experiments.experiment import BaseExperiment
 class CNN2RNNExperiment(BaseExperiment):
 
     def should_save(self, epoch: int) -> bool:
-        return not ((epoch + 1) % 2) or super().should_save(epoch)
+        return bool(not epoch % 2) or super().should_save(epoch)
