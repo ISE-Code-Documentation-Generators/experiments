499b969a78251743d7154f5971d068577859ba93
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-03-12 11:50:19 +0330

    refactor(evaluator_test-mock): "extract function" for 'cached_property'

--- ise_cdg_experiments/cnn2rnn/evaluator_test/mock.py
+++ ise_cdg_experiments/cnn2rnn/evaluator_test/mock.py
@@ -1,16 +1,16 @@
-from typing import TYPE_CHECKING
-
+from functools import cached_property
+from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
     from ise_cdg_experiments.cnn2rnn.test.mock import MockExperimentFacade
+    from ise_cdg_experiments.cnn2rnn.plotter import CNN2RNNPlotter
+    from ise_cdg_experiments.interfaces import ExperimentVisitorInterface
 
 class SampleCNN2RNNFeaturesEvaluatorContainer:
     def __init__(self, mock_experiment_facade: "MockExperimentFacade") -> None:
         from ise_cdg_experiments.cnn2rnn.evaluator import CNN2RNNFeaturesEvaluator
-        from ise_cdg_experiments.cnn2rnn.plotter import CNN2RNNPlotter
 
-        test_plotter = CNN2RNNPlotter()
-        test_visitors = [test_plotter]
+        test_visitors: List["ExperimentVisitorInterface"] = [self.__plotter]
         test_example_ratio: float = 0.001
         proposed_tester = CNN2RNNFeaturesEvaluator(
             device=mock_experiment_facade.get_device(),
@@ -24,15 +24,16 @@ class SampleCNN2RNNFeaturesEvaluatorContainer:
             example_ratio=test_example_ratio,
         )
 
-        self.__test_plotter = test_plotter
         self.__proposed_tester = proposed_tester
+
+    @cached_property
+    def __plotter(self) -> "CNN2RNNPlotter":
+        from ise_cdg_experiments.cnn2rnn.plotter import CNN2RNNPlotter
+        return CNN2RNNPlotter()
 
     def __get_evaluator(self):
         return self.__proposed_tester
-
-    def __get_plotter(self):
-        return self.__test_plotter
     
     def get_metrics(self):
         self.__get_evaluator().valid_for_save()
-        return self.__get_plotter().to_plot
+        return self.__plotter.to_plot
